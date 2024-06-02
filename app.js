3803ba5bf3c705be13cbb2938bf31486deef0c27
sonamChopheld <sonamchopheld@gmail.com>
2024-06-02 20:13:04 +0600

    cm

--- app.js
+++ app.js
@@ -3,59 +3,24 @@ require("dotenv").config();
 
 const path = require("path");
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//required routes
+const clientRouter = require("./routes/client");
 
 //serve stactic file
 app.use(express.static("public"));
 
-// user routes
-const userRoutes = require("./routes/userRoutes");
-const adminRoutes = require("./routes/admin_profileRoutes");
-const applicantProfile = require("./routes/applicant_ProfileRoutes");
-const applicant = require("./routes/applicantRoutes");
-
-// client routes
-const clientProfile = require("./routes/client_profileRoutes");
-const client = require("./routes/clientRoutes");
-const payment_record = require("./routes/payment_recordRoutes");
-const project = require("./routes/projectRoutes");
-const talentProfile = require("./routes/talent_profileRoutes");
-const talent = require("./routes/talentRoutes");
-
-const edit_talent = require("./routes/edit_talent_profileRoutes");
-const hire_request = require("./routes/hire_requestRoutes");
-const search = require("./routes/searchRoutes");
-const view = require("./routes/viewRoutes");
-
-// Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/mydatabase");
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-
 // Middleware
 app.use(express.json()); // Parse JSON bodies
+//mongo connect
+connectDB();
+const connectDB = require("./config/db");
+const { route } = require("./routes/client_profileRoutes");
 
-// Routes
-app.use("/api/users", userRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/applicantProfile", applicantProfile);
-app.use("/api/applicant", applicant);
-app.use("/api/clientProfile", clientProfile);
-app.use("/api/client", client);
-app.use("/api/payments", payment_record);
-app.use("/api/projects", project);
-app.use("/api/talentProfile", talentProfile);
-app.use("/api/talent", talent);
-app.use("/api/edit_profile", edit_talent);
-app.use("/api/hire_request", hire_request);
-app.use("/api/search", search);
-app.use("/api/view", view);
+//client router
+app.use("/client", clientRouter);
 
 app.get("/", (req, res) => {
   res.sendFile("views/index.html", { root: "public" });
@@ -64,3 +29,48 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// unused route
+// user routes
+// const userRoutes = require("./routes/userRoutes");
+// const adminRoutes = require("./routes/admin_profileRoutes");
+// const applicantProfile = require("./routes/applicant_ProfileRoutes");
+// const applicant = require("./routes/applicantRoutes");
+
+// // client routes
+// const clientProfile = require("./routes/client_profileRoutes");
+// const client = require("./routes/clientRoutes");
+// const payment_record = require("./routes/payment_recordRoutes");
+// const project = require("./routes/projectRoutes");
+// const talentProfile = require("./routes/talent_profileRoutes");
+// const talent = require("./routes/talentRoutes");
+
+// const edit_talent = require("./routes/edit_talent_profileRoutes");
+// const hire_request = require("./routes/hire_requestRoutes");
+// const search = require("./routes/searchRoutes");
+// const view = require("./routes/viewRoutes");
+
+// Connect to MongoDB
+// mongoose.connect("mongodb://localhost:27017/mydatabase");
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// db.once("open", () => {
+//   console.log("Connected to MongoDB");
+// });
+
+// Routes
+// app.use("/api/users", userRoutes);
+// app.use("/api/admin", adminRoutes);
+// app.use("/api/applicantProfile", applicantProfile);
+// app.use("/api/applicant", applicant);
+// app.use("/api/clientProfile", clientProfile);
+// app.use("/api/client", client);
+// app.use("/api/payments", payment_record);
+// app.use("/api/projects", project);
+// app.use("/api/talentProfile", talentProfile);
+// app.use("/api/talent", talent);
+// app.use("/api/edit_profile", edit_talent);
+// app.use("/api/hire_request", hire_request);
+// app.use("/api/search", search);
+// app.use("/api/view", view);
--- routes/client_profileRoutes.js
+++ routes/client_profileRoutes.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const clientController = require('../controllers/client_profileController');
+const controller = require("../controllers/client_profileController");
 
 // Create a new client
-router.post('/clients', clientController.createClient);
+router.post("/clients", controller.createClient);
 
 // Get a client by ID
-router.get('/clients/:userId', clientController.getClientById);
+router.get("/clients/:userId", controller.getClientById);
 
 // Update a client
-router.put('/clients/:userId', clientController.updateClient);
+router.put("/clients/:userId", controller.updateClient);
 
 // Delete a client
-router.delete('/clients/:userId', clientController.deleteClient);
+router.delete("/clients/:userId", controller.deleteClient);
 
 module.exports = router;
