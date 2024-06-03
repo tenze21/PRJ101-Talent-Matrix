95137a7800b3cd3add6219d751128a346452c9f5
sonamChopheld <sonamchopheld@gmail.com>
2024-06-04 05:33:39 +0600

    same

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -6,6 +6,8 @@ const adminController = require("../controllers/admin_controller");
 router.get("/get_talents", adminController.get_talent);
 router.get("/get_talents_short", adminController.get_talent_short);
 
+router.get("/get_pro_com", adminController.get_pro_com);
+
 // router.post("/login_user", userController.loginUser);
 
 module.exports = router;
