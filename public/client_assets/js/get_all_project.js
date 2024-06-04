54714af38911e83ac2a40c4082726cbbf6e65a31
sonamChopheld <sonamchopheld@gmail.com>
2024-06-04 07:52:11 +0600

    commit

--- public/client_assets/js/get_all_project.js
+++ public/client_assets/js/get_all_project.js
@@ -27,6 +27,7 @@ window.onload = function () {
     })
     .then((data) => {
       data = data[0];
+
       console.log(data);
       // Assuming the response data has the fields `profile`, `fullname`, and `role`
       //   const profilePictureUrl = data.profile || "/client_assets/images/default_profile.svg"; // Use default if not provided
@@ -43,96 +44,168 @@ window.onload = function () {
   fetch(`/project/get_project_email/${userEmail}`)
     .then((res) => res.json())
     .then((data) => {
+      // console.log(data);
+      showProject(data);
+    });
+
+  //////////////profile
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+
+  // get client data
+
+  fetch(`/client/get_profile_email/${userEmail}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json(); // Assuming the response contains the user data
+      } else {
+        return res.json().then((data) => {
+          alert(`Failed to load profile: ${data.message}`);
+          throw new Error(data.message);
+        });
+      }
+    })
+    .then((data) => {
       console.log(data);
-      showProject(data)
+
+      data = data[0];
+      console.log(data);
+      // Assuming the response data has the fields `profile`, `fullname`, and `role`
+      //   const profilePictureUrl = data.profile || "/client_assets/images/default_profile.svg"; // Use default if not provided
+      const clientName = data.fullname;
+      const userType = data.role;
+
+      // Update the DOM elements with the fetched data
+      //   document.getElementById("profile_picture").src = profilePictureUrl;
+      document.getElementById("client_name").textContent = clientName;
+      document.getElementById("user_type").textContent = userType;
+      if (data.profile_img.data) {
+        var base64String = arrayBufferToBase64(data.profile_img.data);
+        // Set the image source
+        const wrapper = document.querySelector(".profile");
+        const img = document.createElement("img");
+        img.src = `data:image/jpeg;base64,${base64String}`;
+        img.classList.add("client_img");
+        wrapper.appendChild(img);
+
+        const imgWrapper = document.querySelector(".img_wrapper");
+        const img2 = document.createElement("img");
+        img2.src = `data:image/jpeg;base64,${base64String}`;
+        imgWrapper.appendChild(img2);
+
+        const defaultProfile1 = document.getElementById("default_profile1");
+        const defaultProfile2 = document.getElementById("default_profile2");
+        defaultProfile1.style.display = "none";
+        defaultProfile2.style.display = "none";
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching profile:", error);
     });
 };
 
-function showProject(prj){
-    prj.forEach(project=>{
-        const main = document.querySelector(".projects_wrapper");
-         const section = document.createElement("section");
-         section.setAttribute("id", "pro_card");
-         section.setAttribute("aria-labelledby", "project_title");
-         section.className = "project_wrapper";
+function showProject(prj) {
+  prj.forEach((project) => {
+    const main = document.querySelector(".projects_wrapper");
+    const section = document.createElement("section");
+    section.setAttribute("id", "pro_card");
+    section.setAttribute("aria-labelledby", "project_title");
+    section.className = "project_wrapper";
 
-         const btnsDiv = document.createElement("div");
-         btnsDiv.className = "btns";
+    const btnsDiv = document.createElement("div");
+    btnsDiv.className = "btns";
 
-         const hireTalentLink = document.createElement("a");
-         hireTalentLink.href = "/views/client/talents.html";
-         hireTalentLink.className = "hire_talent";
-         hireTalentLink.textContent = "Hire Talents";
+    const hireTalentLink = document.createElement("a");
+    hireTalentLink.href = "/views/client/talents.html";
+    hireTalentLink.className = "hire_talent";
+    hireTalentLink.textContent = "Hire Talents";
 
-         const projectEditLink = document.createElement("a");
-         projectEditLink.href = "project_creation.html";
-         projectEditLink.className = "project_edit";
-         projectEditLink.innerHTML = '<i class="bx bx-edit-alt"></i>';
+    const projectEditLink = document.createElement("a");
+    projectEditLink.href = "project_creation.html";
+    projectEditLink.className = "project_edit";
+    projectEditLink.innerHTML = '<i class="bx bx-edit-alt"></i>';
 
-         const projectTitle = document.createElement("h3");
-         projectTitle.setAttribute("id", "project_title");
-         projectTitle.textContent = project.prj_title;
+    const projectTitle = document.createElement("h3");
+    projectTitle.setAttribute("id", "project_title");
+    projectTitle.textContent = project.prj_title;
 
-         const projectIdP = document.createElement("p");
-         projectIdP.innerHTML = `Project ID: <span id="project_id_value">${project._id}</span>`;
+    const projectIdP = document.createElement("p");
+    projectIdP.innerHTML = `Project ID: <span id="project_id_value">${project._id}</span>`;
 
-         const projectDescription = document.createElement("p");
-         projectDescription.setAttribute("id", "project_description");
-         projectDescription.textContent = project.prj_description;
+    const projectDescription = document.createElement("p");
+    projectDescription.setAttribute("id", "project_description");
+    projectDescription.textContent = project.prj_description;
 
-         const projectScope = document.createElement("p");
-         projectScope.setAttribute("id", "project_scope");
-         if(project.prj_scope=="small"){
-             projectScope.textContent = "Small(1-2 months)";
-         }else if(project.prj_scope=="medium"){
-            projectScope.textContent = "Medium(3-5 months)";
-         }else{
-            projectScope.textContent = "Large(6+ months)";
-         }
+    const projectScope = document.createElement("p");
+    projectScope.setAttribute("id", "project_scope");
+    if (project.prj_scope == "small") {
+      projectScope.textContent = "Small(1-2 months)";
+    } else if (project.prj_scope == "medium") {
+      projectScope.textContent = "Medium(3-5 months)";
+    } else {
+      projectScope.textContent = "Large(6+ months)";
+    }
 
-         const paymentP = document.createElement("p");
-         paymentP.innerHTML = `Nu. <span id="payment">${project.prj_payment}</span>`;
+    const paymentP = document.createElement("p");
+    paymentP.innerHTML = `Nu. <span id="payment">${project.prj_payment}</span>`;
 
-         const skillsDiv = document.createElement("div");
-         skillsDiv.className = "skills";
-         const skills=project.prj_skills.split(",");
-         skills.forEach(skill=>{
-            const p=document.createElement("p");
-            p.setAttribute("id", "skill");
-            p.textContent=skill;
-            skillsDiv.appendChild(p);
-         })
+    const skillsDiv = document.createElement("div");
+    skillsDiv.className = "skills";
+    const skills = project.prj_skills.split(",");
+    skills.forEach((skill) => {
+      const p = document.createElement("p");
+      p.setAttribute("id", "skill");
+      p.textContent = skill;
+      skillsDiv.appendChild(p);
+    });
 
-         const markAsDoneDiv = document.createElement("div");
-         markAsDoneDiv.className = "mark_as_done_btn";
+    const markAsDoneDiv = document.createElement("div");
+    markAsDoneDiv.className = "mark_as_done_btn";
 
-         const markAsDoneBtn = document.createElement("button");
-         markAsDoneBtn.className = "open_confirmation_modal";
-         markAsDoneBtn.textContent = "Mark As Done";
+    const markAsDoneBtn = document.createElement("button");
+    markAsDoneBtn.className = "open_confirmation_modal";
+    markAsDoneBtn.textContent = "Mark As Done";
 
-         const hr = document.createElement("hr");
-         hr.style.marginBottom = "20px";
+    const hr = document.createElement("hr");
+    hr.style.marginBottom = "20px";
 
-         // Append elements
-         btnsDiv.appendChild(hireTalentLink);
-         btnsDiv.appendChild(projectEditLink);
+    // Append elements
+    btnsDiv.appendChild(hireTalentLink);
+    btnsDiv.appendChild(projectEditLink);
 
-         markAsDoneDiv.appendChild(markAsDoneBtn);
+    markAsDoneDiv.appendChild(markAsDoneBtn);
 
-         section.appendChild(btnsDiv);
-         section.appendChild(projectTitle);
-         section.appendChild(projectIdP);
-         section.appendChild(projectDescription);
-         section.appendChild(projectScope);
-         section.appendChild(paymentP);
-         section.appendChild(skillsDiv);
-         section.appendChild(markAsDoneDiv);
+    section.appendChild(btnsDiv);
+    section.appendChild(projectTitle);
+    section.appendChild(projectIdP);
+    section.appendChild(projectDescription);
+    section.appendChild(projectScope);
+    section.appendChild(paymentP);
+    section.appendChild(skillsDiv);
+    section.appendChild(markAsDoneDiv);
 
-         main.appendChild(section);
-         main.appendChild(hr);
+    main.appendChild(section);
+    main.appendChild(hr);
 
-         const noProject=document.getElementById("no_pro");
-         noProject.style.display="none";
+    const noProject = document.getElementById("no_pro");
+    noProject.style.display = "none";
+  });
+}
 
-    })
+function arrayBufferToBase64(buffer) {
+  let binary = "";
+  let bytes = new Uint8Array(buffer);
+  let len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
 }
